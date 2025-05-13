1d93548c5d6c4d9e065ade2892b6704aad1a06cb
davidddeveloper <programmingdonebydavid@gmail.com>
2025-05-13 15:57:53 +0000

    created login and signup route, updated the create account in signup page to getting started

---
+++ app/get-started/page.tsx
@@ -0,0 +1,15 @@
+export default function GetStartedPage() {
+  return (
+    <main className="flex flex-col items-center justify-center w-full h-screen md:flex-row">
+        <div className="flex flex-col items-center justify-center w-full h-screen">
+            <h1 className="text-4xl font-bold">Getting Started</h1>
+            <p className="mt-4 text-lg">To get started with LIA Create An Account / login!</p>
+        </div>
+
+        <div>
+            {/*<img src="" alt="" srcset="" className="" />*/}
+        </div>
+    </main>
+    
+  )
+}

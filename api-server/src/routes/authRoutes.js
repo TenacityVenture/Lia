b364ef545f63c663022a7124b80cccd56973fb33
Samuel Koigor F. Turay <96601470+Koigor97@users.noreply.github.com>
2025-05-20 10:31:56 +0200

    add the refresh-token route

--- api-server/src/routes/authRoutes.js
+++ api-server/src/routes/authRoutes.js
@@ -1,12 +1,29 @@
 // src/routes/promptRoutes.js
 const express = require('express');
 const router = express.Router();
-const {  loginUser } = require('../controllers/authController');
-const { registerUser } = require('../controllers/authController');
 
+const {  
+    loginUser, 
+    registerUser, 
+    refreshAccessToken
+} = require('../controllers/authController');
+
+// POST /api/auth/sign-in -> login and get access token
 router.post('/sign-in', loginUser);
 
+// POST /api/auth/register -> create new user and receive access token
 router.post('/register', registerUser);
 
+/**
+ * Since we are managing authentication server-side, the refresh-token endpoint
+ * must be part of the same auth router. This keeps all related logic together.
+ * It eliminates duplication, and lets the middleware and error handling stay 
+ * clean and organized.
+ */
+
+
+
+// POST /auth/refresh-token → refresh access token using cookie-based refresh_token
+router.post('/refresh-token', refreshAccessToken);
 
 module.exports = router;

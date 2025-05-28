4ea0e7d6df04e1cd26ad544b9d4e7238d3947c9f
Samuel Koigor F. Turay <96601470+Koigor97@users.noreply.github.com>
2025-05-28 17:28:35 +0200

    export app from server

--- api-server/src/server.js
+++ api-server/src/server.js
@@ -65,3 +65,5 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = app; // Export the app for testing purposes

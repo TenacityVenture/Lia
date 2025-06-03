c6118895c2500056c2124f1f67e5dcde8c2e5d42
davidddeveloper <programmingdonebydavid@gmail.com>
2025-06-03 09:38:47 +0000

    updated content.js to clear the jwts if the event.data.type received from the website after logout is equal to CLEAR_JWTs

--- extension/content.js
+++ extension/content.js
@@ -26,13 +26,19 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
 // Listen for auth tokens after sign-in or signup on the website
 window.addEventListener('message', (event) => {
-  if (event.origin !== 'https://getlia.live') return;
+  if (event.origin !== 'https://www.getlia.live') return;
 
   if (event.data.type === 'SEND_JWTs') {
     chrome.runtime.sendMessage({
       type: 'STORE_JWTs',
       access_token: event.data.access_token,
       refresh_token: event.data.refresh_token
+    });
+  }
+
+  if (event.data.type === 'CLEAR_JWTs') {
+    chrome.storage.local.remove(['access_token', 'refresh_token'], () => {
+      console.log('Access token and refresh token cleared from storage.');
     });
   }
 });

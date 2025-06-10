e66ef4c92854ad8b478651d57a47ff95f467ab41
davidddeveloper <programmingdonebydavid@gmail.com>
2025-06-10 01:14:22 +0000

    added the /improve route for aiImprovePost

--- api-server/src/routes/promptRoutes.js
+++ api-server/src/routes/promptRoutes.js
@@ -5,10 +5,13 @@ const { rewritePost } = require('../controllers/promptController');
 const { authenticate } = require('../middlewares/authMiddleware');
 
 const { suggestReply } = require('../controllers/promptController');
+const { aiImprovePost } = require('../controllers/promptController');
 
 
 router.post('/rewrite', authenticate, rewritePost);
 
 router.post('/suggest-reply', authenticate, suggestReply);
+
+router.post('/improve', authenticate, aiImprovePost);
 
 module.exports = router;

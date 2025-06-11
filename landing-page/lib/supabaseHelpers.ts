0b55c30ddc49209ca151592c6c40e3d4e462303f
davidddeveloper <programmingdonebydavid@gmail.com>
2025-06-11 01:36:07 +0000

    create a new helper refreshTokenDirectlyWithSupabase

--- landing-page/lib/supabaseHelpers.ts
+++ landing-page/lib/supabaseHelpers.ts
@@ -15,4 +15,18 @@ const signInDirectlyWithSupabase = async (email: string, password: string) => {
   return {access_token: data.session.access_token, refresh_token: data.session.refresh_token}
 }
 
-export { signInDirectlyWithSupabase }
+const refreshTokenDirectlyWithSupabase = async (refresh_token: string) => {
+  const { data, error } = await supabase.auth.refreshSession({
+    refresh_token
+  })
+
+  // checks if there was an error during sign in
+  if (error) {
+    throw Error('Invalid Refresh Token')
+  }
+
+  // return access token and refresh token
+  return {access_token: data.session?.access_token, refresh_token: data.session?.refresh_token}
+}
+
+export { signInDirectlyWithSupabase, refreshTokenDirectlyWithSupabase }

c9c98127da7374984d2cd92796721ae636efc077
davidddeveloper <programmingdonebydavid@gmail.com>
2025-06-25 16:53:26 +0000

    updated dashboard with tailwind style for the logout button

--- landing-page/components/dashboard/dashboard-header.tsx
+++ landing-page/components/dashboard/dashboard-header.tsx
@@ -127,7 +127,7 @@ export default function DashboardHeader() {
               <DropdownMenuSeparator />
               <DropdownMenuItem className="cursor-pointer">
                 <LogOut className="mr-2 h-4 w-4" />
-                <Link href={"/logout"}>Log out</Link>
+                <Link href={"/logout"} className="w-full h-full">Log out</Link>
               </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>

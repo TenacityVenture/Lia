8dd51fadea6ffb150e4a40aa32330e781811122a
davidddeveloper <programmingdonebydavid@gmail.com>
2025-06-26 20:38:18 +0000

    add the activity route

--- api-server/src/routes/usageRoutes.js
+++ api-server/src/routes/usageRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { authenticate } = require('../middlewares/authMiddleware');
 const { getUsageStats } = require('../controllers/usageController');
+const { getRecentActivity } = require('../controllers/usageController')
 
 router.get('/stats', authenticate, getUsageStats);
+router.get('/activity', authenticate, getRecentActivity)
 
 module.exports = router;

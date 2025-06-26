eb2da82514eb7c93b578c294038c50e031db2d3d
davidddeveloper <programmingdonebydavid@gmail.com>
2025-06-26 23:26:58 +0000

    added credentions: include as part of the request headers

--- landing-page/components/login-form.tsx
+++ landing-page/components/login-form.tsx
@@ -45,7 +45,7 @@ export function LoginForm() {
     // This would be where you'd call your API to authenticate the user
     console.log(values)
 
-    // Simulate API call
+
     //await new Promise((resolve) => setTimeout(resolve, 1500))
     const apiUrl:string = `${process.env.NEXT_PUBLIC_API_HOST}/api/auth/sign-in`
     await fetch(apiUrl, {
@@ -53,6 +53,7 @@ export function LoginForm() {
       headers: {
         "Content-Type": "application/json",
       },
+      credentials: 'include',
       body: JSON.stringify(values),
     })
       .then((res) => res.json())
--- landing-page/components/signup-form.tsx
+++ landing-page/components/signup-form.tsx
@@ -56,6 +56,7 @@ export function SignupForm() {
       headers: {
         "Content-Type": "application/json",
       },
+      credentials: 'include',
       body: JSON.stringify(values),
     })
       .then((res) => {

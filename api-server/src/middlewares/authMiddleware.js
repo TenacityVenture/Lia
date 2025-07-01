efc41eb66e203f94e96e12e0f5d229adf847d623
davidddeveloper <programmingdonebydavid@gmail.com>
2025-07-01 22:50:38 +0000

    update authMiddleware by removing the getPaypalAccessToken function entirely from the file there cause it does not make sense for it to be there. Rather I have moved it to services/getPaypalAccessToken

--- api-server/src/middlewares/authMiddleware.js
+++ api-server/src/middlewares/authMiddleware.js
@@ -21,37 +21,6 @@ const authenticate = (req, res, next) => {
   }
 };
 
-// get access token from PayPal
-// for subsequent API calls
-const getPaypalAccessToken = async () => {
-  const base64Credentials = Buffer.from(
-    `${process.env.PAYPAL_CLIENT_ID}:${process.env.PAYPAL_CLIENT_SECRET}`
-  ).toString('base64');
-
-  const response = await fetch(`${process.env.PAYPAL_BASE_URL}/v1/oauth2/token`, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Accept-Language': 'en_US',
-      'Authorization': `Basic ${base64Credentials}`,
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: 'grant_type=client_credentials',
-  });
-
-  if (!response.ok) {
-    throw new Error(`Failed to get access token: ${response.status} ${response.statusText}`);
-  }
-
-  const data = await response.json();
-
-  if (!data.access_token) {
-    throw new Error('No access token received from PayPal');
-  }
-
-  return data.access_token;
-}
-
 const checkPlan = (requiredPlan = 'free') => {
   return async (req, res, next) => {
     const user = req.user;

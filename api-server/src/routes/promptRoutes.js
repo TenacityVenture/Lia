482cf0217975e9b41baec566407c1ef087bde624
davidddeveloper <programmingdonebydavid@gmail.com>
2025-07-01 19:22:47 +0000

    added the checkPlan middleware as part of the /rewrite /suggest-reply and /improve request to check the user has the necessary plan to access the service

--- api-server/src/routes/promptRoutes.js
+++ api-server/src/routes/promptRoutes.js
@@ -3,15 +3,16 @@ const express = require('express');
 const router = express.Router();
 const { rewritePost } = require('../controllers/promptController');
 const { authenticate } = require('../middlewares/authMiddleware');
+const { checkPlan } = require('../middlewares/authMiddleware');
 
 const { suggestReply } = require('../controllers/promptController');
 const { aiImprovePost } = require('../controllers/promptController');
 
 
-router.post('/rewrite', authenticate, rewritePost);
+router.post('/rewrite', authenticate, checkPlan('standard'), rewritePost);
 
-router.post('/suggest-reply', authenticate, suggestReply);
+router.post('/suggest-reply', authenticate, checkPlan('standard'), suggestReply);
 
-router.post('/improve', authenticate, aiImprovePost);
+router.post('/improve', checkPlan('standard'), authenticate, aiImprovePost);
 
 module.exports = router;

37dada06ee0b5eacb16117d3fd2fe7f30791afd8
davidddeveloper <programmingdonebydavid@gmail.com>
2025-07-20 04:09:31 +0000

    create a new routes for enhancing notes

--- api-server/src/routes/promptRoutes.js
+++ api-server/src/routes/promptRoutes.js
@@ -8,7 +8,7 @@ const { checkPlan } = require('../middlewares/authMiddleware');
 const { suggestReply } = require('../controllers/promptController');
 const { aiImprovePost } = require('../controllers/promptController');
 const { aiSendMessage } = require('../controllers/promptController');
-
+const { enhanceNote } = require('../controllers/promptController');
 
 router.post('/rewrite', authenticate, checkPlan('standard'), rewritePost);
 
@@ -17,5 +17,7 @@ router.post('/suggest-reply', authenticate, checkPlan('standard'), suggestReply)
 router.post('/improve', authenticate, checkPlan('standard'), aiImprovePost);
 
 router.post('/message', authenticate, checkPlan('standard'), aiSendMessage);
+
+router.post('enhance-note', authenticate, checkPlan('standard'), enhanceNote);
 
 module.exports = router;

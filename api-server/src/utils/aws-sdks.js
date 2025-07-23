599b0f4a0fa9aae0071b20331844f33574e760a4
koigor97 <turaysamuel101@yahoo.com>
2025-07-23 15:16:56 +0200

    rename file and add AWS S3 client package

---
+++ api-server/src/utils/aws-sdks.js
@@ -0,0 +1,33 @@
+const { PollyClient, SynthesizeSpeechCommand } = require("@aws-sdk/client-polly");
+const { S3Client } = require("@aws-sdk/client-s3");
+require('dotenv').config();
+
+const pollyClient = new PollyClient({
+    // eu-west-1 is ireland
+  region: "us-east-1", // N. Virginia for long-form/generative voices 
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    sessionToken: process.env.AWS_SESSION_TOKEN, // Optional, if using temporary credentials
+  },
+});
+
+// console.log('Polly client initialized with region:', pollyClient);
+
+
+const s3Client = new S3Client({
+  region: "eu-west-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    sessionToken: process.env.AWS_SESSION_TOKEN,
+  }
+})
+
+// console.log("S3Client initialized with region:", s3Client);
+
+
+module.exports = {
+  pollyClient,
+  s3Client,
+}

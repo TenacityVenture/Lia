41d10303aa04d4c6b4c341939544405597be06d9
davidddeveloper <programmingdonebydavid@gmail.com>
2025-07-31 15:03:14 +0000

    typeWriter effect utility function to be used in content.js

--- extension/utils/helpers.js
+++ extension/utils/helpers.js
@@ -153,7 +153,21 @@ async function setIfChanged(area = 'local', newData = {}) {
   }
 }
 
+// typewriter effect function
+async function typeWriter(plainContent, element) {
+  if (i <= plainContent.length) {
+    const currentText = plainContent.slice(0, i);
+    element.innerHTML = currentText + (i < plainContent.length ? '<span class="lia-cursor">|</span>' : '');
+    i++;
+    setTimeout(typeWriter, 10);
+  } else {
+    // Final formatting
+    element.innerHTML = plainContent;
+  }
+}
+
 window.getLinkedinUserInfo = getLinkedinUserInfo
 window.getLiaUserInfo = getLiaUserInfo
 window.getMe = getMe
 window.detectLinkedInTheme = detectLinkedInTheme
+window.typeWriter = typeWriter
